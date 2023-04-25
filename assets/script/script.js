c681f015268a77f9a56f145299223b8678b67f0d
Malik <malikemiroglu92@gmail.com>
2023-04-25 11:56:50 +0300

    boş satırlar silindi

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+// ------ Dropdown Menu ------
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu() {
+    subMenu.classList.toggle("openMenu");
+}
+
+// ------ Dropdown Menu End ------
